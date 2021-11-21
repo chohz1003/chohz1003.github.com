c9c6bc55c26e1feed20ea5be117d826313549c85
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2021-11-21 17:51:19 +0900

    Update 2021-11-21-cmind-drawingcode.py

--- _posts/cmind/2021-11-21-cmind-drawingcode.py
+++ _posts/cmind/2021-11-21-cmind-drawingcode.py
@@ -7,9 +7,9 @@ import socket,threading
 import pyautogui
 from worrd import gud
 
-def recvMsg(soc):
+def recvMsg(soc): #좌표 받음
     while True:
-        data = soc.recv(15)
+        data = soc.recv(15) #길이를 
         msg = data.decode()
         a=msg.split(',')
         ex.sok(a[0],a[1],a[2],a[3])
@@ -38,7 +38,7 @@ def main():
 client_socket  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket .connect(("localhost", 4444))
 
-class MyApp(QMainWindow):
+class MyApp(QMainWindow): #창 
     xy=[]
 
     def __init__(self):
@@ -53,10 +53,10 @@ class MyApp(QMainWindow):
     def print_label(self, vbutton, vlabel):
         vlabel.setText(vbutton.text())
 
-    def initUI(self):
+    def initUI(self): #창 설정
         self.setWindowTitle('Simple Painter')
-        self.setGeometry(300, 200, 400, 400)
-        self.intialL1 = 'First Label'
+        self.setGeometry(300, 200, 400, 400) 
+        self.intialL1 = 'First Label' 
         a=gud()
         button = QPushButton(a[0], self)
         button.move(150, 0)
@@ -81,23 +81,22 @@ class MyApp(QMainWindow):
             a=str(self.last_point)+str(e.pos())
             painter.drawLine(self.last_point, e.pos()) #  (1칸전,현재)              drawline(x1, y1, x2, y2)
             self.last_point = e.pos() #마우스커서좌표
-            #self.update()
             b = re.findall("\d+", a)
             del b[0]
             del b[2]
             c = ''
             for a in b:
                 a=str(a)
-                k=a.zfill(3)
+                k=a.zfill(3) #길이를 
                 c = c + str(k) + ','
-            c=c[:-1]       #좌표
+            c=c[:-1] #좌표
             b = []
             u=re.findall("\d+", str(self.geometry()))
             del u[0]
             self.xy=u
             i = re.findall("\d+", str(pyautogui.position()))
-            if int(i[0]) >= int(u[0]) and int(i[0]) <= int(u[0])+int(u[2]) and int(i[1]) >= int(u[1])+30 and int(i[1]) <= int(u[1])+int(u[3])-40:
-                client_socket.send(c.encode())
+            if int(i[0]) >= int(u[0]) and int(i[0]) <= int(u[0])+int(u[2]) and int(i[1]) >= int(u[1])+30 and int(i[1]) <= int(u[1])+int(u[3])-40:#그려지는 좌표 제한
+                client_socket.send(c.encode()) #좌표 전송
 
     def sok(self, q,w,e,r):
         painter = QPainter(self.image)
@@ -105,7 +104,7 @@ class MyApp(QMainWindow):
         painter.drawLine(int(q), int(w),int(e),int(r)) #  (1칸전,현재)              drawline(x1, y1, x2, y2)
         self.update()
 
-    def draw_line(self, qp):
+    def draw_line(self, qp): 
         qp.setPen(QPen(Qt.white, 40))
         qp.drawLine(0, 380, 400, 380)
         qp.setPen(QPen(Qt.white, 37))
@@ -114,6 +113,6 @@ class MyApp(QMainWindow):
 if __name__ == '__main__':
     main()
     app = QApplication(sys.argv)
-    ex = MyApp()
+    ex = MyApp() #창
     sys.exit(app.exec_())
     client_socket.close()
