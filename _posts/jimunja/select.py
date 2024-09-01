2802d2541967abbbd5bee2087a0e0b3504b8e399
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2024-09-01 12:56:24 +0900

    Update select.py

--- _posts/jimunja/select.py
+++ _posts/jimunja/select.py
@@ -1,25 +1,16 @@
 import pymysql
-from PIL import Image
-import base64
-from io import BytesIO
-import matplotlib.pyplot as plt
 
-conn = pymysql.connect(host='183.99.87.90',
-        user='root',
-        password='swhacademy!',
-        db='hangcheol',
-        charset='utf8')
-try:
-    with conn.cursor() as cursor:
-        sql = 'SELECT 이미지 FROM img'
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        #print(type(results[0][0]))
-        #print(type(results))
-        img = Image.open(BytesIO(base64.b64decode(results[0][0])))
-        print(img)
-        plt.imshow(img)
-        plt.show()
-
-finally:
-    conn.close()
+def selectt(word):
+    conn = pymysql.connect(host='183.99.87.90',
+            user='root',
+            password='swhacademy!',
+            db='hangcheol',
+            charset='utf8')
+    try:
+        with conn.cursor() as cursor:
+            sql = f"SELECT * FROM img WHERE 문자 = '{word}'"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            return results[0][1]
+    finally:
+        conn.close()
