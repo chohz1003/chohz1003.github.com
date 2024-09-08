80523e3ee0e50377c64c0a639c083b20fd306283
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2024-09-08 13:00:50 +0900

    Update jimunja_select.py

--- _posts/jimunja/jimunja_select.py
+++ _posts/jimunja/jimunja_select.py
@@ -1,22 +1,31 @@
 import pymysql
 
-def select(word):
+def select(word_list):
+    w_list = []
     conn = pymysql.connect(host='183.99.87.90',
             user='root',
             password='swhacademy!',
             db='hangcheol',
             charset='utf8')
     try:
-        with conn.cursor() as cursor:
-            fake_sql = f"SELECT IFNULL(MAX('문자'), 0) '문자' FROM img WHERE 문자 = '{word}'"
-            cursor.execute(fake_sql)
-            results = cursor.fetchall()
-            if results[0][0] == '0':
-                sql = "SELECT * FROM img WHERE 문자 = '다시입력'"
-            else:
-                sql = f"SELECT * FROM img WHERE 문자 = '{word}'"
-            cursor.execute(sql)
-            results = cursor.fetchall()
-            return results[0][1].decode('utf-8')
+        for words in word_list:
+            for word in words:
+                if word == ' ':
+                    continue
+                with conn.cursor() as cursor:
+                    if word_list == '입력':
+                        sql = "SELECT * FROM img WHERE 문자 = '입력'"
+                    else:
+                        fake_sql = f"SELECT IFNULL(MAX('문자'), 0) '문자' FROM img WHERE 문자 = '{word}'"
+                        cursor.execute(fake_sql)
+                        results = cursor.fetchall()
+                        if results[0][0] == '0':
+                            sql = "SELECT * FROM img WHERE 문자 = '다시입력'"
+                        else:
+                            sql = f"SELECT * FROM img WHERE 문자 = '{word}'"
+                    cursor.execute(sql)
+                    results = cursor.fetchall()
+                    w_list.append(results[0][1].decode('utf-8'))
+        return w_list
     finally:
         conn.close()
