252e01d5687c80bc763a644d7f8d580c2ed3ced2
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2024-10-13 15:23:44 +0900

    Create businformation.py

---
+++ _posts/businformation/businformation.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def bus(num):
+    response = requests.get(f'https://api.gbis.go.kr/ws/rest/busrouteservice/page?serviceKey=1234567890&pageSize=20&pageNo=1&keyword={num}')
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    bus=list()
+    all = soup.select('routeList > busRouteList')
+    for one in all:
+        onebus = dict()
+        onebus['지역']= one.select('regionName')[0].get_text()
+        onebus['끝'] = one.select('edStaNm')[0].get_text()
+        onebus['시작'] = one.select('stStaNm')[0].get_text()
+        bus.append(onebus)
+    return bus
