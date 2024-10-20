fb5144aa65dec3cff470b860df4607240b4f32c9
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2024-10-20 15:53:09 +0900

    Update businformation1.py

--- _posts/businformation/businformation1.py
+++ _posts/businformation/businformation1.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def bus(num):
-    response = requests.get(f'https://api.gbis.go.kr/ws/rest/busrouteservice/page?serviceKey=1234567890&pageSize=20&pageNo=1&keyword={num}')
+    response = requests.get(f'https://api.gbis.go.kr/ws/rest/busrouteservice/page?serviceKey=1234567890&pageSize=20&pageNo=1&keyword={num}')#8
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     bus=list()
