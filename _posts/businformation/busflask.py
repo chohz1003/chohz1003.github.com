6b7fe941f99f7ef69bafeab6ce20160f0eb8d1fe
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2024-10-20 15:46:45 +0900

    Update busflask.py

--- _posts/businformation/busflask.py
+++ _posts/businformation/busflask.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for
+from klkl import bus
 
 app = Flask(__name__)
 
@@ -12,6 +13,13 @@ def search():
     if request.method == 'POST':
         return render_template('searchbus.html')
 
-
-if __name__ == "__main__":
-    app.run(debug = True)
+@app.route('/fas',methods = ['POST', 'GET'])
+def fas():
+    if request.method == 'POST':
+        message = list(request.form.listvalues())[0][0]
+        a = bus(message)
+        c=[]
+        for b in a:
+            c.append(b['시작'] + '~' + b['끝'] + '\n첫차 -, 막차 -, 배차 -분\n' + b['지역'])
+        print(c[0])
+        return render_template("fas.html", result=c)
