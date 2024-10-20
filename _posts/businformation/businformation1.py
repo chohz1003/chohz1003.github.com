563a0ebcd9cb7bd72f48a69b5086475353a4b9f8
chohz1003 <61077961+chohz1003@users.noreply.github.com>
2024-10-20 15:47:21 +0900

    Update and rename businformation.py to businformation1.py

--- _posts/businformation/businformation1.py
+++ _posts/businformation/businformation1.py
@@ -12,5 +12,7 @@ def bus(num):
         onebus['지역']= one.select('regionName')[0].get_text()
         onebus['끝'] = one.select('edStaNm')[0].get_text()
         onebus['시작'] = one.select('stStaNm')[0].get_text()
+        onebus['종류'] = one.select('routeTypeName')[0].get_text()
+        onebus['id'] = one.select('routeId')[0].get_text()
         bus.append(onebus)
     return bus
